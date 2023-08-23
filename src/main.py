5a8c8ecaee930939c16ff4d7cb28a2bef72fc49b
PAFFx <jj2jom@gmail.com>
2023-08-23 22:29:02 +0700

    add get todos and get todo/{todo_id}

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, responses
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
@@ -12,7 +12,18 @@ async def init_db():
     client = AsyncIOMotorClient("mongodb://root:rootpassword@localhost:27017")
     await init_beanie(database=client.todos)
 
+
+
 @app.get('/todos')
 async def get_todos() -> list[Todo]:
     todos = await Todo.find().to_list()
     return todos
+
+@app.get('/todos/{todo_id}',status_code= 200)
+async def get_todo(todo_id: str, response: Response) -> Todo | str:
+    todo = await Todo.get(todo_id)
+    if todo == None:
+        response.status_code = 404
+        return "Not Found"
+    return todo
+
